3d965a93f3d70f68c51f14416c766651de4ce5ab
kanghana1 <qazplm12093@naver.com>
2026-05-31 22:48:44 +0900

    feat: 로직 수정 및 마이그레이션

--- functions/routes/statsRoutes.js
+++ functions/routes/statsRoutes.js
@@ -1,13 +1,17 @@
 const { onRequest } = require('firebase-functions/v2/https');
 const { getHomeStats } = require('../services/statsService');
 const { verifyToken } = require('../middlewares/authMiddleware');
-const { getWeekKey } = require('../utils/dateUtils');
+const { getWeekKey, getNextWeekKey, getDayKST } = require('../utils/dateUtils');
 
 const getHomeStatsHandler = onRequest({ cors: true }, async (req, res) => {
   if (req.method !== 'GET') return res.status(405).send('Method Not Allowed');
   const decoded = await verifyToken(req, res);
   if (!decoded) return;
-  const weekKey = req.query.weekKey || getWeekKey();
+
+  // weekKey 없으면 일요일 여부 고려해서 자동 계산
+  const weekKey =
+    req.query.weekKey || (getDayKST() === 0 ? getNextWeekKey() : getWeekKey());
+
   try {
     const result = await getHomeStats(weekKey);
     res.status(200).json(result);
